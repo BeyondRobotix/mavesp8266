33462e40e4c85fcb25d01c1bb469b34b9a92f802
Andrew Tridgell <andrew@tridgell.net>
2018-05-01 22:00:06 +1000

    avoid duplicate version of mavlink functions

--- lib/mavlink/mavlink.h
+++ lib/mavlink/mavlink.h
@@ -1,7 +1,6 @@
 //-- Gymnastics necessary to make this build system include the right file.
 
 #define MAVLINK_COMM_NUM_BUFFERS 2
+#define MAVLINK_SEPARATE_HELPERS
 
 #include "../../generated/mavlink/ardupilotmega/mavlink.h"
-
-
