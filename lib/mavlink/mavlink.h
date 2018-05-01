8b500e34966ac3649d3d36e7dd97fb348238f20a
Andrew Tridgell <andrew@tridgell.net>
2018-05-01 15:20:58 +1000

    use generated mavlink headers

--- lib/mavlink/mavlink.h
+++ lib/mavlink/mavlink.h
@@ -1,3 +1,5 @@
 //-- Gymnastics necessary to make this build system include the right file.
-#include "../../modules/mavlink/include/ardupilotmega/mavlink.h"
+
+#include "../../generated/mavlink/ardupilotmega/mavlink.h"
+
 
