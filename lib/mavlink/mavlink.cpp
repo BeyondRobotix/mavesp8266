429c85f4a3bfa3f9947fb71763b02637a80b1135
Gus Grubba <mavlink@grubba.com>
2018-05-16 15:31:31 -0400

    Remove residual submodule files

--- lib/mavlink/mavlink.cpp
+++
@@ -1,8 +0,0 @@
-#include "mavlink.h"
-
-#ifdef MAVLINK_SEPARATE_HELPERS
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wmissing-declarations"
-#include "../../generated/mavlink/mavlink_helpers.h"
-#pragma GCC diagnostic pop
-#endif
