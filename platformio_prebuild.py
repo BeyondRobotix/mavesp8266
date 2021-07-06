ccf8904a1c3c973b7313ed4c95b21b29bcbad11c
Xelack <alex0505@live.fr>
2021-07-06 08:48:50 +0200

    Add workaround to build on VS/Pio (Windows)

--- platformio_prebuild.py
+++ platformio_prebuild.py
@@ -1,9 +1,26 @@
+import datetime
+import subprocess
+
 Import("env")
+
+# get revivion from Git
+revision = (
+    subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
+    .strip()
+    .decode("utf-8")
+)
+#get current date and time
+curr_date = datetime.datetime.now()
+date_str = f"{curr_date.year}-{curr_date.month:02}-{curr_date.day:02}"
+time_str = f"{curr_date.hour:02}:{curr_date.minute:02}:{curr_date.second:02}"
+
+# add revision and date/time as build informations to the existing build flags (workaround to fix compilation issue with "date +%%..." )
+build_flags = env['BUILD_FLAGS']
+build_flags[0] = "!echo" + ' "-DPIO_SRC_REV="' +  str(revision)  + ' "-DPIO_BUILD_DATE="' + date_str + ' "-DPIO_BUILD_TIME="' +  time_str + " " + build_flags[0]
 
 # retrieve build flags
 my_flags = env.ParseFlags(env['BUILD_FLAGS'])
 defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
-
 version_string = defines.get("VERSION_STRING")  # e.g. "1.2.2"
 board_name = env["BOARD"]  # e.g. "esp01m"
 
