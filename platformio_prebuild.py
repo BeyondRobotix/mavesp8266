e263df14f60bac6ad5c7098523de8b4d11da4e43
Samuel Knox <47692324+SamJCKnox@users.noreply.github.com>
2022-10-24 12:16:20 +0100

    added platformio windows fix

--- platformio_prebuild.py
+++ platformio_prebuild.py
@@ -1,4 +1,22 @@
+import datetime
+import subprocess
+
 Import("env")
+
+revision = (
+    subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
+    .strip()
+    .decode("utf-8")
+)
+#get current date and time
+curr_date = datetime.datetime.now()
+date_str = f"{curr_date.year}-{curr_date.month:02}-{curr_date.day:02}"
+time_str = f"{curr_date.hour:02}:{curr_date.minute:02}:{curr_date.second:02}"
+
+# add revision and date/time as build informations to the existing build flags (workaround to fix compilation issue with "date +%%..." )
+build_flags = env['BUILD_FLAGS']
+build_flags[0] = "!echo" + ' "-DPIO_SRC_REV="' +  str(revision)  + ' "-DPIO_BUILD_DATE="' + date_str + ' "-DPIO_BUILD_TIME="' +  time_str + " " + build_flags[0]
+
 
 # retrieve build flags
 my_flags = env.ParseFlags(env['BUILD_FLAGS'])
